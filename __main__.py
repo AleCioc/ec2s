9de874cc87e3f1f57d7c335e13ce6f5d3b3ddaf9
AleCioc <alessandro@ciociola.me>
2019-08-27 18:59:33 +0200

    moved only hub

--- __main__.py
+++ __main__.py
@@ -7,5 +7,8 @@ city_name = sys.argv[1]
 # from ModelValidation.model_validation import run_model_validation
 # run_model_validation(city_name)
 
-from MultipleRun.multiple_runs import multiple_runs
-multiple_runs(city_name)
+# from MultipleRun.multiple_runs import multiple_runs
+# multiple_runs(city_name)
+
+from MultipleRun.only_hub import run_only_hub
+run_only_hub(city_name)
