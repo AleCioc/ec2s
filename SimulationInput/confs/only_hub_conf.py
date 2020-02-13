6c68426ffe6560020de443921ab2fcef5de146d2
AleCioc <alessandro@ciociola.me>
2020-02-14 00:36:19 +0100

    modify only hub conf

--- SimulationInput/confs/only_hub_conf.py
+++ SimulationInput/confs/only_hub_conf.py
@@ -13,7 +13,7 @@ sim_scenario_conf_grid = {
 
     "hub": [True],
     "hub_zone_policy": ["num_parkings"],
-    "n_poles_n_cars_factor" : np.arange(0.05, 0.2, 0.01),
+    "n_poles_n_cars_factor" : np.arange(0.05, 0.1, 0.001),
 
     "relocation": [False],
     "finite_workers": [False],
