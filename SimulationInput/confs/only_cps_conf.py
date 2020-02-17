e887e122bd19931d506ed9f56b8fb626dcfa8ab2
AleCioc <alessandro@ciociola.me>
2020-02-18 00:53:59 +0100

    modified configs

--- SimulationInput/confs/only_cps_conf.py
+++ SimulationInput/confs/only_cps_conf.py
@@ -8,12 +8,12 @@ sim_scenario_conf_grid = {
 
     "time_estimation": [True],
     "queuing": [True],
-    "alpha": np.arange(2.5, 45, 2.5),
+    "alpha": np.arange(5, 45, 5),
     "beta": np.arange(70, 105, 10),
 
     "hub": [False],
     "hub_zone_policy": ["num_parkings"],
-    "n_poles_n_cars_factor" : [0.1],
+    "n_poles_n_cars_factor": np.arange(0.01, 0.2, 0.03),
 
     "relocation": [False],
     "finite_workers": [False],
@@ -24,6 +24,6 @@ sim_scenario_conf_grid = {
 
     "system_cps": [True],
     "user_contribution": [True],
-    "willingness": [0],
+    "willingness": [0, 0.33, 0.66, 0.99],
 
 }
