3090c80f9ccdf8d99b97dc26e94b6ab34427847c
AleCioc <alessandro@ciociola.me>
2020-02-17 23:27:43 +0100

    modified configs

--- SimulationInput/confs/only_hub_conf.py
+++ SimulationInput/confs/only_hub_conf.py
@@ -8,12 +8,12 @@ sim_scenario_conf_grid = {
 
     "time_estimation": [True],
     "queuing": [True],
-    "alpha": np.arange(5, 45, 5),
-    "beta": np.arange(70, 105, 10),
+    "alpha": [15],
+    "beta": [100],
 
     "hub": [True],
     "hub_zone_policy": ["num_parkings"],
-    "n_poles_n_cars_factor" : [1],
+    "n_poles_n_cars_factor": np.arange(0.1, 1, 0.1),
 
     "relocation": [False],
     "finite_workers": [False],
